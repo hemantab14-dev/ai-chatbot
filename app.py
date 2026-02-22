6868d00a52af99b04f96d0f51c95975b7ddcc80a
Aaron <hemanta.b14@gmail.com>
2026-02-22 15:26:55 +0530

    Fixed missing os import

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from openai import OpenAI
@@ -26,7 +27,7 @@ def chat():
 
     return jsonify({"reply": reply})
 
-import os
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
